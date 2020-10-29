99df27c43d0b8e9f4046e579878ee55b9bac01ca
Tibs92 <tibor.keri@whitestuff.com>
2020-10-29 11:25:27 +0000

    Auth - Find user by ID

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,3 +9,17 @@ const config = require('config')
 
 const { check, validationResult } = require('express-validator');
 
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error')
+	}
+});
