f393ca1bff441bf89586c0efe2c620a47caa81a4
Tibs92 <tibor.keri@whitestuff.com>
2020-10-31 14:11:37 +0000

    Build social objects and create or update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -71,9 +71,40 @@ router.post(
       profileFields.skills = skills.split(",").map((skill) => skill.trim());
     }
 
-    console.log(profileFields.skills);
+    // Build social object
 
-    res.send("Hello");
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    console.log(profileFields.social.twitter);
+
+    // Update data
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Update
+        proifle = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+      await Profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
