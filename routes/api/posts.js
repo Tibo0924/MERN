845cbb33806cf9fb87f9ae063139e0615f2f84c9
Tibs92 <tibor.keri@whitestuff.com>
2020-11-15 16:05:03 +0000

    Route - Post like & unlike posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -58,8 +58,7 @@ router.get("/", auth, async (req, res) => {
 
 router.get("/:id", auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.param.id);
-
+    const post = await Post.findById(req.params.id);
     if (!post) {
       return res.status(404).json({ msg: "Post not found" });
     }
@@ -77,7 +76,7 @@ router.get("/:id", auth, async (req, res) => {
 // @desc    Delete a post
 // @access  Private
 
-router.delete("/", auth, async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
@@ -101,4 +100,31 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   PUT api/post/like/:id
+// @desc    Delete a post
+// @access  Private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked
+
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
