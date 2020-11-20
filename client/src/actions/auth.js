50a019a40ebbe6c1d0a651b5a35dcf3649b88315
Tibs92 <tibor.keri@whitestuff.com>
2020-11-20 17:32:34 +0000

    Dynamic navbar for guest/registered users

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
@@ -84,4 +85,12 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout /Clear Profile
+
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
