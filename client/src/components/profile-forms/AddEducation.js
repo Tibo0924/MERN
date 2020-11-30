00749548b47982e718a82283ae3524f4af3e6824
Tibs92 <tibor.keri@whitestuff.com>
2020-11-30 11:13:45 +0000

    Add education

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -93,7 +93,7 @@ const AddEducation = ({ addEducation, history }) => {
               name="current"
               value=""
             />{" "}
-            Current Job
+            Current
           </p>
         </div>
         <div className="form-group">
